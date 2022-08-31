69724f9262e75edd935c765cc84aae49a422bbba
Ivan Zovkić <izovkic@tvz.hr>
2022-08-31 22:46:15 +0100

    setupirana i18n

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -3,11 +3,40 @@ import { useDispatch, useSelector } from 'react-redux';
 import { clearCurrentUser } from '../store/actions/user';
 import { Role } from '../models/role';
 import logo from './Skypark_logo.png';
+import EngFlag from './eng.png';
+import CroFlag from './cro.png';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState, useEffect, useRef } from 'react';
+import { Button } from 'react-bootstrap';
+import { I18nProvider, LOCALES } from "../i18n";
+import { FormattedMessage, IntlProvider } from "react-intl";
+import React, { Component } from "react";
 
 const NavBar = () => {
 
     const currentUser = useSelector(state => state.user);
+    const [lang, setLang] = useState(LOCALES.CROATIAN);
+
+    useEffect(() => {
+
+        if (localStorage.getItem("language") == null) {
+            localStorage.setItem("language", LOCALES.CROATIAN);
+        }
+
+    }, []);
+
+    const onLanguageChanged = (e) => {
+        e.preventDefault();
+        if (localStorage.getItem("language") == LOCALES.CROATIAN) {
+            localStorage.setItem("language", LOCALES.ENGLISH);
+            setLang(LOCALES.CROATIAN)
+
+        } else {
+            localStorage.setItem("language", LOCALES.CROATIAN);
+            setLang(LOCALES.ENGLISH)
+        }
+    };
+
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -18,70 +47,81 @@ const NavBar = () => {
     };
 
     return (
+        <React.Fragment>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <a href="https://reactjs.org" className="navbar-brand ms-1">
-                <img src={logo} className="App-logo" alt="logo" />
-            </a>
+            <I18nProvider locale={localStorage.getItem("language")}>
+                <a href="https://reactjs.org" className="navbar-brand ms-1">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </a>
 
-            <div className="navbar-nav me-auto">
-                {currentUser?.role === Role.ADMIN &&
-                    <NavDropdown title="Admin" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="/admin-reservations">Reservations</NavDropdown.Item>
-                        <NavDropdown.Item href="/admin-users">Users</NavDropdown.Item>
-                        <NavDropdown.Item href="/admin-parkings">Parkings</NavDropdown.Item>
-                    </NavDropdown>
+                <div className="navbar-nav me-auto">
+                    {currentUser?.role === Role.ADMIN &&
+                        <NavDropdown title="Admin" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/admin-reservations">Reservations</NavDropdown.Item>
+                            <NavDropdown.Item href="/admin-users">Users</NavDropdown.Item>
+                            <NavDropdown.Item href="/admin-parkings">Parkings</NavDropdown.Item>
+                        </NavDropdown>
+                    }
+
+                    <li className="nav-item">
+                        <NavLink to="/home" className="nav-link">
+                            <FormattedMessage id='home'/>
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/parking-calculator" className="nav-link">
+                            Calculator
+                        </NavLink>
+                    </li>
+                    <li className="nav-item" style={{ backgroundColor: 'cadetblue', marginLeft: 20 + 'px', color: 'white' }}>
+
+                        <Button onClick={(e) => onLanguageChanged(e)} className="nav-link" style={{ color: 'white' }} >
+                            {lang} <img src={`${localStorage.getItem("language") === LOCALES.ENGLISH ? CroFlag : EngFlag}`} className="lang-logo" alt="logo" />
+                        </Button>
+                    </li>
+                </div>
+
+
+                {!currentUser &&
+                    <div className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <NavLink to="/register" className="nav-link">
+                                Sign Up
+                            </NavLink>
+                        </li>
+                        <li className="nav-item" style={{ marginRight: 20, marginLeft: 20 }}>
+                            <NavLink to="/login" className="nav-link">
+                                Sign In
+                            </NavLink>
+                        </li>
+                    </div>
                 }
 
-                <li className="nav-item">
-                    <NavLink to="/home" className="nav-link">
-                        Home
-                    </NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/parking-calculator" className="nav-link">
-                        Calculator
-                    </NavLink>
-                </li>
-            </div>
+                {currentUser &&
+                    <div className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <NavLink to="/profile" className="nav-link">
+                                {currentUser.username}
+                                {currentUser?.role === Role.ADMIN && <span> (admin)</span>}
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/change-password" className="nav-link">
+                                Change password
+                            </NavLink>
+                        </li>
 
-            {!currentUser &&
-                <div className="navbar-nav ms-auto">
-                    <li className="nav-item">
-                        <NavLink to="/register" className="nav-link">
-                            Sign Up
-                        </NavLink>
-                    </li>
-                    <li className="nav-item" style={{ marginRight: 20, marginLeft: 20 }}>
-                        <NavLink to="/login" className="nav-link">
-                            Sign In
-                        </NavLink>
-                    </li>
-                </div>
-            }
+                        <li className="nav-item">
+                            <a href="#" className="nav-link" style={{ marginRight: 20, marginLeft: 20 }} onClick={() => logout()}>
+                                Sign Out
+                            </a>
+                        </li>
+                    </div>
+                }
 
-            {currentUser &&
-                <div className="navbar-nav ms-auto">
-                    <li className="nav-item">
-                        <NavLink to="/profile" className="nav-link">
-                            {currentUser.username}
-                            {currentUser?.role === Role.ADMIN && <span> (admin)</span>}
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/change-password" className="nav-link">
-                            Change password
-                        </NavLink>
-                    </li>
-
-                    <li className="nav-item">
-                        <a href="#" className="nav-link" style={{ marginRight: 20, marginLeft: 20 }} onClick={() => logout()}>
-                            Sign Out
-                        </a>
-                    </li>
-                </div>
-            }
-
+            </I18nProvider>
         </nav>
+        </React.Fragment>
     );
 };
 
---
+++ src/i18n/messages/en.js
@@ -0,0 +1,59 @@
+import { LOCALES } from "../locales"
+
+export default {
+
+    [LOCALES.ENGLISH]: {
+        'login': 'Login',
+        'logout' : 'Logout',
+        'register': 'Register',
+        'details_mismatch': "Details do not match!",
+
+        'change_profile_picture': "Change profile picture",
+        'change': "Change",
+        'first_name': "First name",
+        'last_name': 'Last name',
+        'full_name' : 'Full name',
+        'address': 'Address',
+        'username': 'Username',
+        'email': 'Email',
+        'phone': 'Phone',
+        'password': 'Password',
+        'bio' : 'Bio',
+        'rented_vehicles' : 'Rented vehicles',
+        'details' : 'Details',
+        'rental_info' : 'Rental info',
+        'my_info' : 'My info',
+        'rental_date' : 'Rental date',
+        'return_date' : 'Return date',
+        
+        'admin' : 'Admin',
+        'users' : 'Users',
+        'vehicles' : 'Vehicles',
+        'home': 'Home',
+        'about_us': 'About us',
+        'example': 'Example',
+        'profile': 'Profile',
+
+        'change_image': 'Change image',
+        'image': 'Image',
+        'model': 'Model',
+        'm10r': 'Manufacturer',
+        'type': 'Type',
+        'price': 'Price',
+        'mileage' : 'Mileage',
+        'production_year' : 'Production year',
+
+        'app_name' : 'Rent a Car',
+
+        'learn_more': 'Learn more',
+        'add_new' : 'Add new',
+        'edit_info' : 'Edit Info',
+        'return' : 'Return',
+        'rent' : 'Rent',
+        'delete' : 'Delete',
+        'new_vehicle' : 'New Vehicle',
+        'save' : 'Save',
+        'cancel' : 'Cancel',
+        'close' : 'Close',
+    }
+}
